a4ea02558a3c988e1a8b8a0e203e932d9decee97
benkywenk <77287657+benkywenk@users.noreply.github.com>
2021-10-26 11:52:42 +0100

    Update run_param_file.py

--- run_param_file.py
+++ run_param_file.py
@@ -1,5 +1,5 @@
 """
-Parameter file for use in the Dedalus rotating Rayleigh-Bernard convection script.
+Parameter file for use in the Dedalus convection script.
 """
 
 import numpy as np
@@ -8,13 +8,9 @@ Lx, Ly, Lz = 4., 4., 1.                       # Domain size
 Nx, Ny, Nz = 64, 64, 32                    # Number of
 Pr = 1.                             # Prandtl number
 Pm = 1.                             # Magnetic Prandtl number
-Ra = 1e6                          # Rayleigh number
-Ek = 1e-2                           # Ekman Number
-phi = np.pi/6.                      # Angle corresponding to latitude
-Np = 0                              # Number of density scale heights
-Q = 0                               # Chandrasekhar number
-m = 1.5                             # Polytropic index
-theta = 1 - np.exp(-Np/m)           # Dimensionaless inverse T scale height
+Ra = 1e7                          # Rayleigh number
+Ek = 2.5e-3                           #Ekman Number
+phi = np.pi/6.                      #Angle corresponding to latitude
 
 initial_timestep = 1e-6                 # Initial timestep
 max_dt = 1e-4                         # max dt
@@ -22,8 +18,8 @@ max_dt = 1e-4                         # max dt
 snapshot_freq = 1.5e-3              # Frequency snapshot files are outputted
 analysis_freq = 2e-4              # Frequency analysis files are outputted
 
-end_sim_time = 2                   # Stop time in simulations units
+end_sim_time = 0.5                 # Stop time in simulations units
 end_wall_time = np.inf              # Stop time in wall time
 end_iterations = np.inf             # Stop time in iterations
 
-run_name = "test1"                  # Run name
+run_name = "1"
