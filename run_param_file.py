03ed81d80b3810920621b0e4a621c9166bbefffe
benkywenk <77287657+benkywenk@users.noreply.github.com>
2021-11-15 12:30:49 +0000

    Update run_param_file.py

--- run_param_file.py
+++ run_param_file.py
@@ -4,22 +4,23 @@ Parameter file for use in the Dedalus convection script.
 
 import numpy as np
 
-Lx, Ly, Lz = 4., 4., 1.                       # Domain size
-Nx, Ny, Nz = 64, 64, 32                    # Number of
-Pr = 1.                             # Prandtl number
-Pm = 1.                             # Magnetic Prandtl number
-Ra = 1e7                          # Rayleigh number
-Ek = 2.5e-3                           #Ekman Number
-phi = np.pi/6.                      #Angle corresponding to latitude
+Lx, Ly, Lz = 4., 4., 2.         # Domain size
+Nx, Ny, Nz = 48, 48, 24           # Number of gridpoints
+Pr = 1.                           # Prandtl number
+Ra = 1e6                          # Rayleigh number
+Ek = 1e-2                         # Ekman Number
+phi = np.pi/4.                    # Angle corresponding to latitude
 
-initial_timestep = 1e-6                 # Initial timestep
-max_dt = 1e-4                         # max dt
+initial_timestep = 1e-6           # Initial timestep
+max_dt = 1e-4                     # max dt
 
-snapshot_freq = 1.5e-3              # Frequency snapshot files are outputted
+tau = 0.1                        # Time averaging interval
+
+snapshot_freq = 1.5e-3            # Frequency snapshot files are outputted
 analysis_freq = 2e-4              # Frequency analysis files are outputted
 
-end_sim_time = 0.5                 # Stop time in simulations units
-end_wall_time = np.inf              # Stop time in wall time
-end_iterations = np.inf             # Stop time in iterations
+end_sim_time = 1.                # Stop time in simulations units
+end_wall_time = np.inf            # Stop time in wall time
+end_iterations = np.inf           # Stop time in iterations
 
 run_name = "1"
